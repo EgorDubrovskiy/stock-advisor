486f2dce56fe9ab3ff40d23d5e444c1c7931d915
Egor Dubrovskiy <egor.dubrovskiy.1998@mail.ru>
2019-03-05 17:03:11 +0300

    Change footer component

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,33 +4,36 @@ import { Link } from 'react-router-dom';
 export default function Footer() {
   return (
     <footer className="page-footer mt-3 d-flex position-absolute w-100 bg-green justify-content-between">
-      <span className="align-items-center d-flex ml-4">
+      <span className="align-items-center d-flex ml-3">
         Stock market website
       </span>
-      <ul className="d-flex align-items-center list-unstyled mb-0 mr-4">
+      <ul className="d-flex align-items-center list-unstyled mb-0 mr-3">
         <li>
-          <Link to="/facebook">
+          <a href="https://vk.com/id205914816" target="_blank">
             <img
-              src="https://img.icons8.com/material/24/000000/facebook.png"
+              src="https://img.icons8.com/ios/24/000000/vk.png"
               alt="Facebook"
             />
-          </Link>
+          </a>
         </li>
         <li className="mx-3 ">
-          <Link to="/google-plus">
+          <a href="https://github.com/EgorDubrovskiy" target="_blank">
             <img
-              src="https://img.icons8.com/ios/24/000000/google-plus.png"
+              src="https://img.icons8.com/ios/24/000000/github.png"
               alt="Google Plus"
             />
-          </Link>
+          </a>
         </li>
         <li>
-          <Link to="/twitter">
+          <a
+            href="https://www.linkedin.com/in/%D0%B5%D0%B3%D0%BE%D1%80-%D0%B4%D1%83%D0%B1%D1%80%D0%BE%D0%B2%D1%81%D0%BA%D0%B8%D0%B9-985619170/"
+            target="_blank"
+          >
             <img
-              src="https://img.icons8.com/ios/24/000000/twitter-filled.png"
+              src="https://img.icons8.com/ios/24/000000/linkedin.png"
               alt="Twitter"
             />
-          </Link>
+          </a>
         </li>
       </ul>
     </footer>
