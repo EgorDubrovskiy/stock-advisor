ba9e5de0a444083732e4a322ab4ec71db6ab23b2
Egor Dubrovskiy <egor.dubrovskiy.1998@mail.ru>
2019-04-01 21:52:31 +0300

    Add a diagram by 5 last years

--- frontend/src/components/Company/Details/index.js
+++ frontend/src/components/Company/Details/index.js
@@ -1,37 +1,81 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import CompanyInfo from './Info';
 import { forbidExtraProps } from 'airbnb-prop-types';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import CompanyInfo from './Info';
 import {
   getCompany,
   removeCompany,
   getCompanyNews
 } from 'redux/actions/company';
 import { addBookmark } from 'redux/actions/bookmarks';
+import { getAvgPrices } from 'redux/actions/price';
+import { Chart } from 'react-google-charts';
+import { getLastYears } from 'utils/date';
 import Loading from 'components/common/Loading';
 
 class CompanyPage extends React.Component {
-  componentDidMount() {
+  constructor(props) {
+    super(props);
+
     this.props.removeCompany();
     const symbol = this.props.match.params.symbol;
-    this.props.getCompany(symbol);
+    this.props.getCompany(symbol).then(() => {
+      this.props.getAvgPrices(this.props.company.current.symbol, this.state.years.toString());
+    });
     this.props.getCompanyNews(symbol);
+
+    this.state = {years: getLastYears(5)};
   }
 
   render() {
-    if (this.props.company.current.symbol) {
+    let avgPrices = this.props.priceData;
+
+    if (!avgPrices.avg.loader) {
+      avgPrices = avgPrices.avg.items.map((avgPrice) => (
+        Object.values(avgPrice)
+      ));
+      let headers =[...this.state.years.map((year) => (year.toString()))];
+      headers.unshift('Сокращенное название компании');
+      avgPrices.unshift(headers);
+
       return (
-        <CompanyInfo
-          isAuthenticated={this.props.user.isAuthenticated}
-          userId={this.props.user.userInfo.id}
-          company={this.props.company.current}
-          addBookmark={this.props.addBookmark}
-          bookmarks={this.props.bookmarks.list}
-          news={this.props.company.news.items[0]}
-          loading={this.props.bookmarks.loader}
-        />
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12 p-3">
+              <CompanyInfo
+                isAuthenticated={this.props.user.isAuthenticated}
+                userId={this.props.user.userInfo.id}
+                company={this.props.company.current}
+                addBookmark={this.props.addBookmark}
+                bookmarks={this.props.bookmarks.list}
+                news={this.props.company.news.items[0]}
+                loading={this.props.bookmarks.loader}
+              />
+            </div>
+            <div className="col-12 p-3">
+              <Chart
+                width={'100%'}
+                height={'400px'}
+                chartType="BarChart"
+                loader={null}
+                data={avgPrices}
+                options={{
+                  title: 'Стоимость акций за последние 5 лет',
+                  chartArea: { width: '50%' },
+                  hAxis: {
+                    title: 'Средняя цена на акцию за год',
+                    minValue: 0,
+                  },
+                  vAxis: {
+                    title: 'Компания',
+                  },
+                }}
+              />
+            </div>
+            </div>
+          </div>
       );
     }
     return <Loading className="absolute-center" />;
@@ -41,14 +85,16 @@ class CompanyPage extends React.Component {
 const mapStateToProps = state => ({
   user: state.user,
   company: state.company,
-  bookmarks: state.bookmarks
+  bookmarks: state.bookmarks,
+  priceData: state.price,
 });
 
 const mapDispatchToProps = {
   getCompany,
   getCompanyNews,
   addBookmark,
-  removeCompany
+  removeCompany,
+  getAvgPrices
 };
 
 export default connect(
